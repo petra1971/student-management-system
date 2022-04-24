0e0157fc17cf937e54fe5dc55190b3f8b2c1f817
Petra Andreasson <petra.andreasson@iths.se>
2022-04-24 19:38:26 +0200

    Removed getJsonException method

--- src/main/java/se/iths/exception/ExceptionMessage.java
+++ src/main/java/se/iths/exception/ExceptionMessage.java
@@ -1,23 +1,17 @@
 package se.iths.exception;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 public class ExceptionMessage {
     public int code;
     public String status;
     public String message;
 
+    public ExceptionMessage(String message) {
+        this.message = message;
+    }
+
     public ExceptionMessage(int code, String status, String message) {
         this.code = code;
         this.status = status;
         this.message = message;
-    }
-
-    public String getJsonException() {
-        GsonBuilder builder = new GsonBuilder();
-        builder.setPrettyPrinting();
-        Gson gson = builder.create();
-        return gson.toJson(this);
     }
 }
